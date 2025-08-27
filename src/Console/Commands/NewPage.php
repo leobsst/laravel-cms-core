787b66eb3c03f9a32d743f29f7f21f612a54c167
LEOBSST <contact@leobsst.fr>
2025-08-27 16:45:49 +0200

    fix class

--- src/Console/Commands/NewPage.php
+++ src/Console/Commands/NewPage.php
@@ -62,8 +62,8 @@ class NewPage extends Command
         $content .= "\n";
         $content .= "namespace Database\Seeders;\n";
         $content .= "\n";
-        $content .= "use App\Models\Page;\n";
-        $content .= "use App\Models\PagesSeo;\n";
+        $content .= "use Leobsst\LaravelCmsCore\Models\Features\Page;\n";
+        $content .= "use Leobsst\LaravelCmsCore\Models\Features\PagesSeo;\n";
         $content .= "use Illuminate\Database\Seeder;\n";
         $content .= "\n";
         $content .= 'class insert'.$fileName."PageSeeder extends Seeder\n";
