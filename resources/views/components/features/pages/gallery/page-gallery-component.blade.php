441c5167009db2309737c13cb231685f05d34b1d
LEOBSST <contact@leobsst.fr>
2025-09-22 19:50:04 +0200

    fix

--- resources/views/components/features/pages/gallery/page-gallery-component.blade.php
+++ resources/views/components/features/pages/gallery/page-gallery-component.blade.php
@@ -1,6 +1,7 @@
 <div>
     <div @class([
         'w-full grid grid-cols-1 gap-4',
+        'md:grid-cols-2' => $gallery->orientation === \Leobsst\LaravelCmsCore\Enums\Features\Pages\PageGalleryOrientation::HORIZONTAL && $media->count() <= 2,
         'md:grid-cols-4' => $gallery->orientation === \Leobsst\LaravelCmsCore\Enums\Features\Pages\PageGalleryOrientation::HORIZONTAL && $media->count() % 2 === 0,
         'md:grid-cols-3' => $gallery->orientation === \Leobsst\LaravelCmsCore\Enums\Features\Pages\PageGalleryOrientation::HORIZONTAL && $media->count() % 2 !== 0,
     ])>
